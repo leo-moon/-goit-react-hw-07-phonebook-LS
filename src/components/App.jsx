6fa924a8e5346f3168110318ecee80edca4effb5
leo-moon <49075905+leo-moon@users.noreply.github.com>
2023-03-05 23:12:23 +0200

    start

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/integration/react';
+
+import Phonebook from '../Modules/Phonebook/Phonebook';
+// import { store, persistor } from '../redux/store';
+import { store} from '../redux/store';
+
+
+export function App() {
+  return (
+ 
+      <Provider store={store}>
+        {/* <PersistGate loading={null} persistor={persistor}> */}
+          <Phonebook />
+        {/* </PersistGate> */}
+      </Provider>
+
+  );
+}
